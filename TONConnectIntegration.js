a4e63354639256318a6fa865a309de973147ff19
Ihor Reshetnykov <ihor.reshetnykov.workonly@gmail.com>
2024-10-29 01:12:01 +0200

    Test TON Wallet integration #3

---
+++ TONConnectIntegration.js
@@ -0,0 +1,24 @@
+let tonConnect;
+
+window.initializeTonConnect = function() {
+    try {
+        console.log("init ton connect")
+        tonConnect = new TonConnectSDK.TonConnect();
+        console.log(`ton connect: ${tonConnect}`);
+    } catch (error) {
+        console.error("Initialization error:", error);
+    }
+}
+
+window.connectWallet = function() {
+    try {
+        console.log("connect wallet call")
+/*        await tonConnect.connectWallet();
+        const walletAddress = tonConnect.wallet?.address || "";
+        if (walletAddress) {
+            SendMessage("TonWalletBridge", "OnWalletConnected", walletAddress);
+        }*/
+    } catch (error) {
+        console.error("Initialization error:", error);
+    }
+}
